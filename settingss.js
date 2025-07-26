5e8b5ecb5025499ffbc423a26eaf788debe3c9f8
mamunm1235586 <mamunmondal3486@gmail.com>
2025-07-27 02:31:29 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~0yEz2RYJ#B-cRXDHzOZ-recy8Hr2gnNNaiFiTGokE9tnH0611Jhs",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
